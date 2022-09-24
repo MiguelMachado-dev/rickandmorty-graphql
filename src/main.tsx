a1ac9a220e09003201b7d0e78608e9e221cc49d5
Miguel Machado <miiguellmp@gmail.com>
2022-09-24 17:53:16 -0300

    feat(react-query): add query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/default */
+
 import React from 'react'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ReactDOM from 'react-dom/client'
+
 import App from './App'
 import './index.css'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 )
